15afead8370f3acdc29b39217c91644f07cc51e0
Takuma Yoshida <yoa.jmpr.w@gmail.com>
2016-01-25 14:56:09 +0900

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,16 @@
 defmodule OneSignal.Mixfile do
   use Mix.Project
 
+  @description "Elixir wrapper of OneSignal"
+
   def project do
     [app: :one_signal,
      version: "0.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package]
   end
 
   # Configuration for the OTP application
@@ -18,15 +21,13 @@ defmodule OneSignal.Mixfile do
      mod: {OneSignal, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp package do
+    [maintainers: ["Takuma Yoshida"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/yoavlt/one_signal"},
+    ]
+  end
+
   defp deps do
     [
       {:poison, "~> 1.5"},
