4909b9affb586a77a5944a7266ccf6954431677a
Takuma Yoshida <yoa.jmpr.w@gmail.com>
2016-02-12 22:52:11 +0900

    Bump version to 0.0.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OneSignal.Mixfile do
 
   def project do
     [app: :one_signal,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
