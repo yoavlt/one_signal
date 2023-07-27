2e90f2e4d4c54be226757f80784e213766937b43
Silas <me@silentsilas.com>
2023-07-27 11:52:01 -0400

    fix bug where library consumer has to specify the httpoison config

--- config/config.exs
+++ config/config.exs
@@ -32,10 +32,9 @@ config :one_signal, OneSignal,
   api_key: "your api key",
   app_id: "your app id",
   legacy_api_key: "your legacy api key",
-  legacy_app_id: "your legacy app id"
+  legacy_app_id: "your legacy app id",
+  get_notification: &HTTPoison.get/2,
+  post_notification: &HTTPoison.post/3,
+  delete_notification: &HTTPoison.delete/2
 
-config :one_signal, OneSignal, get_notification: &OneSignal.FakeHTTPoison.get/2
-config :one_signal, OneSignal, post_notification: &OneSignal.FakeHTTPoison.post/3
-config :one_signal, OneSignal, delete_notification: &OneSignal.FakeHTTPoison.delete/2
-
-import_config "#{Mix.env()}.exs"
+import_config("#{Mix.env()}.exs")
--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,9 @@
 import Config
 
-config :one_signal, OneSignal, notification: OneSignal.Test.FakeNotification
+config :one_signal, OneSignal,
+  get_notification: &OneSignal.FakeHTTPoison.get/2,
+  post_notification: &OneSignal.FakeHTTPoison.post/3,
+  delete_notification: &OneSignal.FakeHTTPoison.delete/2
 
 if File.exists?("config/test.secret.exs") do
   import_config "test.secret.exs"
