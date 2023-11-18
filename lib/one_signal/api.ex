e04a18b24ff90149d6926ee81c03a8bd114d0fe6
Elijah Brandyberry <elijah@brandyberry.us>
2023-11-18 11:54:54 -0500

    Handle errors when response code is 200

--- lib/one_signal/api.ex
+++ lib/one_signal/api.ex
@@ -125,9 +125,9 @@ defmodule OneSignal.API do
 
   defp handle_response(%Response{body: body, status_code: code})
        when code in 200..299 do
-    with {:ok, result} <- Poison.decode(body) do
-      {:ok, result}
-    else
+    case Poison.decode(body) do
+      {:ok, %{"errors" => errors}} -> {:error, errors}
+      {:ok, result} -> {:ok, result}
       {:error, :invalid} -> {:error, {:invalid, "Could not parse invalid body"}}
       {:error, error} -> {:error, error}
       error -> {:error, {:unknown, "An unknown error has occured", error}}
